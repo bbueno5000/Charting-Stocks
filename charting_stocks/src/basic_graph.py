932c6ab60e7657680694a1d907cf92083b885450
B. Bueno <bbueno25@hotmail.com>
2020-01-21 09:41:10 -0700

    edited basic_graph.py

--- charting_stocks/src/basic_graph.py
+++ charting_stocks/src/basic_graph.py
@@ -14,15 +14,26 @@ def graph_data(stock):
             stock_file, delimiter=',', unpack=true, converters={0: mpl_dates.strpdate2num('%Y%m%d')}
             )
         figure = pyplot.figure()
-        axis1 = pyplot.subplot(1, 1, 1)
+        axis1 = pyplot.subplot(2, 1, 1)
         axis1.plot(date, openp)
         axis1.plot(date, highp)
         axis1.plot(date, lowp)
         axis1.plot(date, closep)
+        pyplot.ylabel('Stock Price')
+        axis1.grid(True)
+        axis2 = pyplot.subplot(2, 1, 2, sharex=axis1)
+        axis2.bar(date, volume)
+        pyplot.ylabel('Volume')        
+        axis2.grid(True)
         axis1.xaxis.set_major_locator(mpl_ticker, MaxNLocator(10))
         axis1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%Y-%m-%d'))
         for label in axis1.xaxis.get_ticklabels():
+            label.set_rotation(90)
+        for label in axis2.xaxis.get_ticklabels():
             label.set_rotation(45)
+        pyplot.subplots_adjust(left=0.1, bottom=0.19, right=0.93, top=0.95, wspace=0.2, hspace=0.07)
+        pyplot.xlabel('Date')
+        pyplot.suptitle(stock + 'Stock Price')
         pyplot.show()
     except Exception as e:
         print('main loop', str(e))
