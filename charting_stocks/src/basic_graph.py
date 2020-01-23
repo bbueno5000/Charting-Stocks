cdad7a78a5c02b79655de7eaf9c2d582e1c29802
B. Bueno <bbueno25@hotmail.com>
2020-01-23 01:01:12 -0700

    edited basic_graph.py (Charting Stocks/Forex for Technical Analysis Part 15)

--- charting_stocks/src/basic_graph.py
+++ charting_stocks/src/basic_graph.py
@@ -9,9 +9,9 @@ import time
 
 matplotlib.rcParams.update({'font.size': 9})
 
-each_stock = 'AAPL', 'TSLA'
+each_stock = 'EBAY', 'AAPL', 'TSLA'
 
-def graph_data(stock):
+def graph_data(stock, moving_average_1, moving_average_2):
     try:
         stock_file = stock + '.txt'
         date, closep, highp, lowp, openp, volume = numpy.loadtxt(
@@ -24,10 +24,15 @@ def graph_data(stock):
             append_line = date[x], openp[x], closep[x], highp[x], lowp[x], volume[x]
             candle_args.append(append_line)
             x += 1
+        average_1 = moving_average(closep, moving_average_1)
+        average_2 = moving_average(closep, moving_average_2)
+        starting_point = len(date[moving_average_2-1:])
         figure = pyplot.figure(facecolor='#07000d')
         # axis 1
         axis1 = pyplot.subplot2grid((5,4), (0,0), rowspan=4, colspan=4, axisbg='#07000d')
         candlestick(axis1, candle_args, width=1, colorup='#9eff15', colordown='#f1717')
+        axis1.plot(date[-starting_point:], average_1[-starting_point:])
+        axis1.plot(date[-starting_point:], average_2[-starting_point:])
         axis1.xaxis.set_major_locator(mpl_ticker, MaxNLocator(10))
         axis1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%Y-%m-%d'))
         axis1.grid(True, color='w')
@@ -56,7 +61,7 @@ def graph_data(stock):
         for label in axis2.xaxis.get_ticklabels():
             label.set_rotation(45)
         pyplot.ylabel('Volume', color='w')
-        
+        # super
         pyplot.suptitle(stock + 'Stock Price', color='w')
         pyplot.setp(axis1.get_xticklabels(), visible=False)
         pyplot.subplots_adjust(left=0.9, bottom=0.14, right=0.94, top=0.95, wspace=0.2, hspace=0)
@@ -65,7 +70,12 @@ def graph_data(stock):
     except Exception as e:
         print('main loop', str(e))
 
+def moving_average(values, window):
+    weights = numpy.repeat(1.0, window) / window
+    smas = numpy.convolve(values, weights, 'valid')
+    return smas
+
 if __name__ == '__main__':
 	for stock in each_stock:
-		pull_data(stock)
+		pull_data(stock, 12, 26)
 	time.sleep(500)
