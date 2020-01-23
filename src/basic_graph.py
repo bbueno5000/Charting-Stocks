ef406ebd1eb2a55f90ce7c7981427af02669d7ad
B. Bueno <bbueno25@hotmail.com>
2020-01-23 16:03:18 -0700

    edited basic_graph.py (Charting Stocks and Forex in python Part 17)

--- src/basic_graph.py
+++ src/basic_graph.py
@@ -30,49 +30,57 @@ def graph_data(stock, moving_average_1, moving_average_2):
         label_1 = str(moving_average_1) + ' SMA'
         label_2 = str(moving_average_2) + ' SMA'
         figure = pyplot.figure(facecolor='#07000d')
+        # axis 0
+        axis_0 = pyplot.subplot2grid((5,4), (0,0), rowspan=1, colspan=4, axisbg='#07000d')
+        axis_0.spines['bottom'].set_color('#5998ff')
+        axis_0.spines['top'].set_color('#5998ff')
+        axis_0.spines['left'].set_color('#5998ff')
+        axis_0.spines['right'].set_color('#5998ff')
+        axis_0.tick_params(axis='x', colors='w')
+        axis_0.tick_params(axis='y', colors='w')
+        pyplot.gca().yaxis.set_major_locator(mpl_ticker.MaxNLocator(prune='lower'))
+        pyplot.ylabel('RSI')
         # axis 1
-        axis1 = pyplot.subplot2grid((5,4), (0,0), rowspan=4, colspan=4, axisbg='#07000d')
+        axis_1 = pyplot.subplot2grid((5,4), (1,0), rowspan=4, colspan=4, axisbg='#07000d')
         candlestick(
-            axis1, 
+            axis_1, 
             candle_args[-starting_point:], 
             width=1, 
             colorup='#9eff15', 
             colordown='#f1717'
             )
-        axis1.plot(date[-starting_point:], average_1[-starting_point:], '#5998ff', label=label_1, linewidth=1.5)
-        axis1.plot(date[-starting_point:], average_2[-starting_point:], '#e1edf9', label=label_2, linewidth=1.5)
-        axis1.xaxis.set_major_locator(mpl_ticker, MaxNLocator(10))
-        axis1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%Y-%m-%d'))
-        axis1.grid(True, color='w')
-        axis1.yaxis.label.set_color('w')
-        axis1.spines['bottom'].set_color('#5998ff')
-        axis1.spines['top'].set_color('#5998ff')
-        axis1.spines['left'].set_color('#5998ff')
-        axis1.spines['right'].set_color('#5998ff')
-        axis1.tick_params(axis='y', colors='w')
-        for label in axis1.xaxis.get_ticklabels():
-            label.set_rotation(90)
+        axis_1.plot(date[-starting_point:], average_1[-starting_point:], '#5998ff', label=label_1, linewidth=1.5)
+        axis_1.plot(date[-starting_point:], average_2[-starting_point:], '#e1edf9', label=label_2, linewidth=1.5)
+        axis_1.xaxis.set_major_locator(mpl_ticker, MaxNLocator(10))
+        axis_1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%Y-%m-%d'))
+        axis_1.grid(True, color='w')
+        axis_1.yaxis.label.set_color('w')
+        axis_1.spines['bottom'].set_color('#5998ff')
+        axis_1.spines['top'].set_color('#5998ff')
+        axis_1.spines['left'].set_color('#5998ff')
+        axis_1.spines['right'].set_color('#5998ff')
+        axis_1.tick_params(axis='x', colors='w')
+        axis_1.tick_params(axis='y', colors='w')
+        for label in axis_1.xaxis.get_ticklabels():
+            label.set_rotation(45)
         pyplot.ylabel('Stock Price')
         pyplot.legend(loc=3, fancybox=True, prop={'size': 7})
-        # axis 2
+        # axis 1 volume
         volume_min = 0
-        axis2 = pyplot.subplot2grid((5,4), (4,0), sharex=axis1, rowspan=1, colspan=4, axisbg='#07000d')
-        axis2.plot(date, volume, '#00ffe8', linewidth=0.8)
-        axis2.fill_between(date, volume_min, volume, facecolor='#00ffe8', alpha=0.5)
-        axis2.axes.yaxis.set_ticklabels([])
-        axis2.grid(False)
-        axis2.spines['bottom'].set_color('#5998ff')
-        axis2.spines['top'].set_color('#5998ff')
-        axis2.spines['left'].set_color('#5998ff')
-        axis2.spines['right'].set_color('#5998ff')
-        axis2.tick_params(axis='x', colors='w')
-        axis2.tick_params(axis='y', colors='w')
-        for label in axis2.xaxis.get_ticklabels():
-            label.set_rotation(45)
-        pyplot.ylabel('Volume', color='w')
+        axis_1_volume = axis_1.twinx()
+        axis_1_volume.fill_between(date, volume_min, volume, facecolor='#00ffe8', alpha=0.5)
+        axis_1_volume.axes.yaxis.set_ticklabels([])
+        axis_1_volume.grid(False)
+        axis_1_volume.spines['bottom'].set_color('#5998ff')
+        axis_1_volume.spines['top'].set_color('#5998ff')
+        axis_1_volume.spines['left'].set_color('#5998ff')
+        axis_1_volume.spines['right'].set_color('#5998ff')
+        axis_1_volume.set_ylim(0, 2*volume.max())
+        axis_1_volume.tick_params(axis='x', colors='w')
+        axis_1_volume.tick_params(axis='y', colors='w')
         # super
-        pyplot.suptitle(stock + 'Stock Price', color='w')
-        pyplot.setp(axis1.get_xticklabels(), visible=False)
+        pyplot.suptitle(stock, color='w')
+        pyplot.setp(axis_0.get_xticklabels(), visible=False)
         pyplot.subplots_adjust(left=0.9, bottom=0.14, right=0.94, top=0.95, wspace=0.2, hspace=0)
         pyplot.show()
         figure.savefig('example.png', facecolor=figure.get_facecolor())
